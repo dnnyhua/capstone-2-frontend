3290b04bf69df67e4578a57572f8853041299ee5
Your Name <you@example.com>
2023-06-02 17:05:50 -0700

    added styling for addpetform modal

--- src/forms/AddPetForm.js
+++ src/forms/AddPetForm.js
@@ -44,59 +44,59 @@ const AddPetFormModal = () => {
     <>
       <button className="addPet" onClick={handleModalOpen}>+</button>
 
-      {showModal && (
-        <div className={`modal ${showModal ? 'show' : ''}`}>
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Form Modal</h5>
-                <button type="button" className="close" onClick={handleModalClose}>
-                  <span>&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">
-                <form onSubmit={handleFormSubmit}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name:</label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      value={formData.name}
-                      onChange={handleInputChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="breed">Breed:</label>
-                    <input
-                      type="breed"
-                      id="breed"
-                      name="breed"
-                      value={formData.breed}
-                      onChange={handleInputChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="age">Age:</label>
-                    <textarea
-                      id="age"
-                      name="age"
-                      value={formData.age}
-                      onChange={handleInputChange}
-                    ></textarea>
-                  </div>
-                  <button type="submit">Submit</button>
-                </form>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" onClick={handleModalClose}>
-                  Close
-                </button>
-              </div>
+
+      <div className={`modal ${showModal ? 'show' : ''}`}>
+        <div className="modal-dialog">
+          <div className={`modal-content ${showModal ? 'show' : ''}`}>
+            <div className="modal-header">
+              <h5 className="modal-title">Form Modal</h5>
+              <button type="button" className="close" onClick={handleModalClose}>
+                <span>&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleFormSubmit}>
+                <div className="form-group">
+                  <label htmlFor="name">Name:</label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="breed">Breed:</label>
+                  <input
+                    type="breed"
+                    id="breed"
+                    name="breed"
+                    value={formData.breed}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="age">Age:</label>
+                  <textarea
+                    id="age"
+                    name="age"
+                    value={formData.age}
+                    onChange={handleInputChange}
+                  ></textarea>
+                </div>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" onClick={handleModalClose}>
+                Close
+              </button>
             </div>
           </div>
         </div>
-      )}
+      </div>
+
     </>
   );
 };
