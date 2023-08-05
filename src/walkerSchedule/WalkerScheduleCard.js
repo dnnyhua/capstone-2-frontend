f045d26e9c878d8348e4d36c2bb790c914a480da
Your Name <you@example.com>
2023-08-04 18:33:20 -0700

    removed moment

--- src/walkerSchedule/WalkerScheduleCard.js
+++ src/walkerSchedule/WalkerScheduleCard.js
@@ -5,10 +5,60 @@ import moment from 'moment';
 import dogPawIcon from '../images/dog-paw-icon.png';
 
 
+// const WalkerScheduleCard = ({ id, date, time, duration, status }) => {
+//     const formatedDate = moment(date).format('M-D-YYYY')
+//     const day = moment(formatedDate).format('dddd');
+//     const formattedTime = moment(time, 'HH:mm').format('h:mm A');
+
+//     return (
+//         <Link to={`/schedule/${id}`} >
+//             <div className="WalkerScheduleCard" id={id}>
+//                 <div className="row ">
+//                     <img src={dogPawIcon} className="dogPawIcon" alt="" />
+//                     <div className="col-md-2 py-3 px-2 dow">
+//                         {day}
+//                     </div>
+//                     <div className="col-md-2 py-3 px-2">
+//                         {formatedDate}
+//                     </div>
+//                     <div className="col-md-2 py-3 px-2">
+//                         {formattedTime}
+//                     </div>
+//                     <div className="col-md-2 py-3 px-2">
+//                         {duration} mins
+//                     </div>
+//                     <div className="col-md-2 py-3 px-2">
+//                         {status}
+//                     </div>
+//                 </div>
+//             </div>
+//         </Link>
+//     )
+// }
+
+// export default WalkerScheduleCard;
+
+
+
+const getFormattedDate = (date) => {
+    const options = { month: 'numeric', day: 'numeric', year: 'numeric' };
+    return new Date(date).toLocaleDateString(undefined, options);
+};
+
+const getFormattedTime = (time) => {
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+    return new Date(`1970-01-01T${time}`).toLocaleTimeString(undefined, options);
+};
+
+const getDayOfWeek = (date) => {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return daysOfWeek[new Date(date).getDay()];
+};
+
 const WalkerScheduleCard = ({ id, date, time, duration, status }) => {
-    const formatedDate = moment(date).format('M-D-YYYY')
-    const day = moment(formatedDate).format('dddd');
-    const formattedTime = moment(time, 'HH:mm').format('h:mm A');
+    const formattedDate = getFormattedDate(date);
+    const day = getDayOfWeek(date);
+    const formattedTime = getFormattedTime(time);
 
     return (
         <Link to={`/schedule/${id}`} >
@@ -19,7 +69,7 @@ const WalkerScheduleCard = ({ id, date, time, duration, status }) => {
                         {day}
                     </div>
                     <div className="col-md-2 py-3 px-2">
-                        {formatedDate}
+                        {formattedDate}
                     </div>
                     <div className="col-md-2 py-3 px-2">
                         {formattedTime}
@@ -33,8 +83,9 @@ const WalkerScheduleCard = ({ id, date, time, duration, status }) => {
                 </div>
             </div>
         </Link>
-    )
-}
+    );
+};
 
 export default WalkerScheduleCard;
+
 
